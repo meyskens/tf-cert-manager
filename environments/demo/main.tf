88f92ade6086ce026a4d715d37bccbe6ead16a2a
Maartje Eyskens <maartje@eyskens.me>
2020-06-19 13:40:40 +0200

    rm ACME
    Signed-off-by: Maartje Eyskens <maartje@eyskens.me>

--- environments/demo/main.tf
+++ environments/demo/main.tf
@@ -1,6 +1,5 @@
 module "demo_cluster" {
   source        = "../../modules/kubernetes"
   cluster_name  = "demo"
-  contact_email = "maartje+tfcm@eyskens.me"
-  is_cluster_up = true
+  is_cluster_up = false
 }
--- modules/kubernetes/variables.tf
+++ modules/kubernetes/variables.tf
@@ -2,10 +2,6 @@ variable "cluster_name" {
   description = "Unique cluster name"
 }
 
-variable "contact_email" {
-  description = "Email for ACME issuer"
-}
-
 variable "is_cluster_up" {
   description = "Is the cluster up?"
   default     = false
